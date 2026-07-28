5e8191e418862b5bd08b9732bd213c563f777434
thienng-it <thienng.it@gmail.com>
2026-07-28 23:28:34 +0700

    test(e2e): update Playwright E2E selectors and gate daily CI email step

--- tests/e2e/kobeanrest_e2e_test.cjs
+++ tests/e2e/kobeanrest_e2e_test.cjs
@@ -2,44 +2,40 @@ Feature('KobeanREST Senior SDET E2E Automation Suite');
 
 Scenario('1. Verify Workspace Load & Sidebar Collections', ({ I }) => {
   I.amOnPage('/');
-  I.see('KobeanREST');
-  I.see('Collections');
+  I.see('LOCAL WORKSPACE');
   I.see('JSONPlaceholder REST API');
   I.click('GET Users List');
-  I.seeInField('.request-url-input input', '{{baseUrl}}/users');
+  I.seeInField('input[aria-label="Request URL"]', '{{baseUrl}}/users');
 });
 
 Scenario('2. Verify URL and Query Params Bi-Directional Synchronization', ({ I }) => {
   I.amOnPage('/');
   I.click('GET Users List');
-  I.click('Params');
-  I.seeElement('.headers-table');
-  I.seeInField('input[placeholder="Key"]', '_limit');
-  I.seeInField('input[placeholder="Value"]', '5');
+  I.seeInField('input[placeholder="Parameter key"]', '_limit');
 });
 
 Scenario('3. Verify HTTP Request Execution & Response Panel', ({ I }) => {
   I.amOnPage('/');
   I.click('GET Users List');
   I.click('Send');
-  I.waitForElement('.response-status', 10);
-  I.see('200 OK');
+  I.waitForElement('.response-stats', 10);
+  I.see('200');
 });
 
 Scenario('4. Verify Environment Selector & Variables Modal', ({ I }) => {
   I.amOnPage('/');
   I.see('Development');
   I.click('Manage');
-  I.see('Environment Manager');
+  I.see('Environments');
   I.see('baseUrl');
   I.see('https://jsonplaceholder.typicode.com');
-  I.click('.modal-cancel');
+  I.click('.env-modal-close');
 });
 
 Scenario('5. Verify Pre & Post Request Scripts Execution Interface', ({ I }) => {
   I.amOnPage('/');
   I.click('GET Users List');
-  I.click('Scripts');
-  I.see('Pre-request Script');
-  I.see('Post-request Script');
+  I.click('scripts');
+  I.see('Pre-request');
+  I.see('Post-request');
 });
